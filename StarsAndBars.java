55cfaa3ac436efdb84b6c1f1b06d6d72ece358b3
aopeters86 <aopeters86@yahoo.com>
2022-12-04 09:42:18 -0600

    Initial commit

---
+++ StarsAndBars.java
@@ -0,0 +1,37 @@
+
+public class StarsAndBars {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//only can decrement by 2?
+		//these need to have 2 more rows under and stars added
+		
+		int n = 6;
+		int n2 = 6;
+		int count = 0;
+		//6 down 5 side
+		
+		//controls rows
+		for(int i =0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+			System.out.print("*   ");
+			}
+			System.out.println();
+	}
+
+}
+}
+//i = 1
+//==*=
+//=*==*==*==*==*=
+//==*=
+//=*==*==*==*==*=
+//==*=
+//=*==*==*==*==*=
+//==*=
+//=*==*==*==*==*=
+//==*=
+//=*==*==*==*==*=
+//==*=
+//=*==*==*==*==*=
