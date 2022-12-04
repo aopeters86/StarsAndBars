291095d7c9467889ee8489c0213a4a9e3593326e
aopeters86 <aopeters86@yahoo.com>
2022-12-04 10:01:44 -0600

    Create program for American Flag
    Try 2

--- StarsAndBars.java
+++ StarsAndBars.java
@@ -7,19 +7,22 @@ public class StarsAndBars {
 		//only can decrement by 2?
 		//these need to have 2 more rows under and stars added
 		
-		int n = 6;
+		int n = 5;
 		int n2 = 6;
-		int count = 0;
+		int n3 = 10;
 		//6 down 5 side
 		
 		//controls rows
 		for(int i =0; i < n; i++) {
 			for(int j = 0; j < n; j++) {
-			System.out.print("*   ");
+			System.out.print("*");
+			//This is adding spaces to the right of the above star
+			System.out.print("    ");
+			
 			}
 			System.out.println();
 	}
-
+//System.out.println();
 }
 }
 //i = 1
