7b688e44a0bbebe470e933e2dc67d01689050018
aopeters86 <114446425+aopeters86@users.noreply.github.com>
2022-12-04 20:43:18 -0600

    Update StarsAndBars3StringsConCat.java
    more updates

--- StarsAndBars3StringsConCat.java
+++ StarsAndBars3StringsConCat.java
@@ -4,36 +4,41 @@ public class StarsAndBars3StringsConCat {
 		// TODO Auto-generated method stub
 		
 		//create american flag with strings
-
+		//55 total char per line
 		
-		String stars6  = "*   *   *   *   *";
-		String stars5  = "  *   *   *   *  ";
-		String noBars  = "                 ";
-		String bars    = "||||||||||||||||||||||||||||||||||"; 
+		String stars6  = "*   *   *   *   *   *";
+		String stars5  = "  *   *   *   *   *  ";
+		String noBars  = "[][][][][][][][][][][][][][][][][]"; //34 char
+		String bars    = "//////////////////////////////////"; //34 char
 		int temp = 0;
-		
+		int temp1 = 0;
 		for (int i = 0; i < 4;i++) {
-			System.out.println(stars6 + bars);
+			System.out.println(stars6 + noBars);
 			if(i % 1 == 0) {
-				System.out.println(stars5 + noBars);
-				
-			}
+				System.out.println(stars5 + bars);
+		}
 		}
 		if(temp != 1) {
 		for(int i = 0; i < 1; i++) {
-			System.out.println(stars6 + bars);
+			System.out.println(stars6 + noBars);
 			temp += 1;
 		}
-			
-//		for(int j = 0; j < 9; j++) {
-//		System.out.println(stars5 + noBars);
-//		}
-			
-	
+		
+		for (int i = 0; i < 2;i++) {
+			System.out.println(bars);
+			if(i % 1 == 0) {
+				System.out.println(noBars);
+			}		
+			}	
+			}
+		if(temp1 != 1) {
+		for(int i = 0; i < 1; i++) {
+			System.out.println(noBars);
+			temp1 += 1;
+		}
 }
 }
 }
-
 
 //		}
 //		//System.out.println(stars5);
@@ -48,3 +53,7 @@ public class StarsAndBars3StringsConCat {
 	//System.out.println();
 	
 	//System.out.println();
+
+//		for(int j = 0; j < 9; j++) {
+//		System.out.println(stars5 + noBars);
+//		}
